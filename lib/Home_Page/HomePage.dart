c90c7bdd4e1bea4eb7710e5bcc9aa28240989430
farhan <farhansadikgalib@gmail.com>
2021-07-22 23:23:15 +0600

    playstore upload

--- lib/Home_Page/HomePage.dart
+++ lib/Home_Page/HomePage.dart
@@ -6,6 +6,256 @@ import 'package:happyvalleyu/Check_Connection/No%20Internet.dart';
 import 'package:happyvalleyu/Push%20Notification/pushNotification.dart';
 import 'package:shimmer/shimmer.dart';
 
+// class HomePage extends StatefulWidget {
+//   final String url;
+//
+//   HomePage({Key? key, required this.url}) : super(key: key);
+//
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+//
+// class _HomePageState extends State<HomePage> {
+//   FirebaseNotifcation? firebase;
+//   int checkInt = 0;
+//   late ConnectivityResult previous;
+//
+//
+//
+//   handleAsync() async {
+//     await firebase!.initialize();
+//     String? token = await firebase!.getToken();
+//     print("Firebase token : $token");
+//   }
+//
+//
+//
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     firebase = FirebaseNotifcation();
+//     handleAsync();
+//
+//     pullToRefreshController = PullToRefreshController(
+//       options: PullToRefreshOptions(color: Colors.yellow[800]),
+//       onRefresh: () async {
+//         if (Platform.isAndroid) {
+//           _webViewController?.reload();
+//         } else if (Platform.isIOS) {
+//           _webViewController?.loadUrl(
+//               urlRequest: URLRequest(url: await _webViewController?.getUrl()));
+//         }
+//       },
+//     );
+//
+//
+//     Connectivity().onConnectivityChanged.listen((ConnectivityResult connresult){
+//       if(connresult == ConnectivityResult.none){
+//         Navigator.pushAndRemoveUntil(
+//             context,
+//             MaterialPageRoute(builder: (context) => No_Internet_Connection()), (route) => false );
+//       }else if(previous == ConnectivityResult.none){
+//         // internet conn
+//         Navigator.pushAndRemoveUntil(
+//             context,
+//             MaterialPageRoute(builder: (context) => No_Internet_Connection()), (route) => false );
+//       }
+//
+//       previous = connresult;
+//     });
+//
+//
+//
+//
+//   }
+//
+//   Future<bool> _onWillPop() async {
+//     return (await showDialog(
+//       context: context,
+//       builder: (context) => new AlertDialog(
+//         title: new Text('Are you sure?'),
+//         content: new Text('Do you want to exit an App'),
+//         actions: <Widget>[
+//           TextButton(
+//             onPressed: () => Navigator.of(context).pop(false),
+//             child: new Text(
+//               'No',
+//               style: TextStyle(color: Colors.green[800]),
+//             ),
+//           ),
+//           TextButton(
+//             onPressed: () => Navigator.of(context).pop(true),
+//             child: new Text(
+//               'Yes',
+//               style: TextStyle(color: Colors.red[800]),
+//             ),
+//           ),
+//         ],
+//       ),
+//     )) ??
+//         false;
+//   }
+//
+//   InAppWebViewController? _webViewController;
+//    double progress = 0;
+//   String url = '';
+//
+//   final GlobalKey webViewKey = GlobalKey();
+//   InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+//       crossPlatform: InAppWebViewOptions(
+//         javaScriptEnabled: true,
+//         useShouldOverrideUrlLoading: true,
+//         useOnDownloadStart: true,
+//       ),
+//       android: AndroidInAppWebViewOptions(
+//         initialScale: 100,
+//         useShouldInterceptRequest: true,
+//         useHybridComposition: true,
+//       ),
+//       ios: IOSInAppWebViewOptions(
+//         allowsInlineMediaPlayback: true,
+//       ));
+//
+//   late PullToRefreshController pullToRefreshController;
+//   final urlController = TextEditingController();
+//
+//   @override
+//   void dispose() {
+//     super.dispose();
+//   }
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return WillPopScope(
+//       onWillPop: _onWillPop,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Color.fromRGBO(30, 166, 154, 1),
+//           title: Shimmer.fromColors(
+//             baseColor: Colors.white,
+//             highlightColor: Color.fromRGBO(40, 166, 154, .8),
+//             child: Column(
+//               children: [
+//                 Text(
+//                   'HappyValleyU',
+//                   textAlign: TextAlign.center,
+//                   style: TextStyle(
+//                     fontSize: 22.0,
+//                     fontFamily: "Poppins",
+//                     // fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           actions: <Widget>[
+//             IconButton(
+//               onPressed: () {
+//                 _webViewController?.goBack();
+//               },
+//               icon: Icon(
+//                 Icons.arrow_back_ios,
+//                 color: Colors.white,
+//               ),
+//             ),
+//             IconButton(
+//               onPressed: () {
+//                 _webViewController?.goForward();
+//               },
+//               icon: Icon(
+//                 Icons.arrow_forward_ios,
+//                 color: Colors.white,
+//               ),
+//             ),
+//             SizedBox(width: 5),
+//           ],
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: () {
+//             _webViewController?.reload();
+//           },
+//           child: Icon(
+//             Icons.refresh, color: Colors.white,
+//
+//           ),
+//           backgroundColor: Color.fromRGBO(30, 166, 154, 1),
+//         ),
+//         body: SafeArea(
+//           child: Container(
+//             child: Column(
+//               children: [
+//                 progress < 1.0
+//                     ? LinearProgressIndicator(
+//                         value: progress,
+//                         backgroundColor: Colors.white,
+//                         valueColor:
+//                             AlwaysStoppedAnimation<Color>(Color.fromRGBO(30, 166, 154, 1)),
+//                       )
+//                     : Center(),
+//                 Expanded(
+//                   child: InAppWebView(
+//                     key: webViewKey,
+//                     initialUrlRequest: URLRequest(
+//                       url: Uri.parse(widget.url),
+//                       headers: {},
+//                     ),
+//                     initialOptions: options,
+//                     pullToRefreshController: pullToRefreshController,
+//                     onWebViewCreated: (controller) {
+//                       _webViewController = controller;
+//                     },
+//                     onLoadStart: (controller, url) {
+//                       setState(() {
+//                         this.url = url.toString();
+//                         urlController.text = this.url;
+//                       });
+//                     },
+//                     androidOnPermissionRequest:
+//                         (controller, origin, resources) async {
+//                       return PermissionRequestResponse(
+//                           resources: resources,
+//                           action: PermissionRequestResponseAction.GRANT);
+//                     },
+//                     onLoadStop: (controller, url) async {
+//                       pullToRefreshController.endRefreshing();
+//                       setState(() {
+//                         this.url = url.toString();
+//                         urlController.text = this.url;
+//                       });
+//                     },
+//                     onLoadError: (controller, url, code, message) {
+//                       pullToRefreshController.endRefreshing();
+//                     },
+//                     onProgressChanged: (controller, progress) {
+//                       if (progress == 100) {
+//                         pullToRefreshController.endRefreshing();
+//                       }
+//                       setState(() {
+//                          this.progress = progress / 100;
+//                         urlController.text = this.url;
+//                       });
+//                     },
+//                     onUpdateVisitedHistory: (controller, url, androidIsReload) {
+//                       setState(() {
+//                         this.url = url.toString();
+//                         urlController.text = this.url;
+//                       });
+//                     },
+//                     onConsoleMessage: (controller, consoleMessage) {
+//                       print(consoleMessage);
+//                     },
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 class HomePage extends StatefulWidget {
   final String url;
 
@@ -98,7 +348,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   InAppWebViewController? _webViewController;
-   double progress = 0;
+  double progress = 0;
   String url = '';
 
   final GlobalKey webViewKey = GlobalKey();
@@ -124,7 +374,6 @@ class _HomePageState extends State<HomePage> {
   void dispose() {
     super.dispose();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -191,7 +440,7 @@ class _HomePageState extends State<HomePage> {
                         value: progress,
                         backgroundColor: Colors.white,
                         valueColor:
-                            AlwaysStoppedAnimation<Color>(Color.fromRGBO(30, 166, 154, 1)),
+                            AlwaysStoppedAnimation<Color>(Colors.yellow[800]!),
                       )
                     : Center(),
                 Expanded(
